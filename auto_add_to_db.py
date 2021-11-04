4d4e873fbe64df15eaaa97b343745c2693596b27
Zalán Horváth <horvathzalan@zal1000.com>
2021-11-05 00:05:45 +0100

    Update auto_add_to_db.py

--- auto_add_to_db.py
+++ auto_add_to_db.py
@@ -1,10 +1,9 @@
 import markdown
-from firebase_admin import credentials
 import firebase_admin
 from firebase_admin import firestore
 import os
-cred = credentials.Certificate(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
-firebase_admin.initialize_app(cred)
+import json 
+firebase_admin.initialize_app(json.loads(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
 f = open("./README.md")
 gkjaséféa = markdown.markdown(f.read())
 checkForLinks = False
